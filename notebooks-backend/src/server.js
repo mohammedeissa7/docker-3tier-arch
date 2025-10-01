dffb24796e43218596047d3edf27cbdac9a7cc9f
Eissa <smme012345@gmail.com>
2025-10-01 16:45:17 +0300

    Add app services to docker compose

--- notebooks-backend/src/server.js
+++ notebooks-backend/src/server.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+app.get('/', (req, res) => res.json({ message:'Hello World!'}));
+
+const port = process.env.PORT;
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+})
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
 });
--- notes-backend/src/server.js
+++ notes-backend/src/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(3001, () => {
-  console.log('Server running on port 3001');
+app.get('/', (req, res) => res.json({ message:'Hello World!'}));
+
+const port = process.env.PORT;
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+})
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB', err);
 });
